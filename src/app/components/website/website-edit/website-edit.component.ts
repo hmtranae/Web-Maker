2372b3f9e845c99d42d1c651d75d9f1a4a245f15
Lilyane Tran <hmtran.ae@gmail.com>
2018-11-05 15:34:10 -0500

    website databinding

--- src/app/components/website/website-edit/website-edit.component.ts
+++ src/app/components/website/website-edit/website-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsiteService } from 'src/app/services/website.service.client';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-website-edit',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteEditComponent implements OnInit {
 
-  constructor() { }
+  websites : any[];
+  uid : string;
+  wid : string;
+  website;
+
+  constructor(private websiteService : WebsiteService, private activatedRoute : ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.uid = params['uid'];
+      this.wid = params['wid'];
+      this.websites = this.websiteService.findWebsitesByUser(this.uid);
+      this.website = this.websiteService.findWebsiteById(this.wid);
+    });
   }
 
+  update() {
+    const newWebsite = {
+      name: this.website.name,
+      description: this.website.description,
+      _id: this.wid,
+      developerId: this.uid
+    };
+
+    this.websiteService.updateWebsite(newWebsite);
+    this.router.navigate(['user', this.uid, 'website'])
+  }
+
+  delete() {
+    this.websiteService.deleteWebsite(this.wid);
+    this.router.navigate(['user', this.uid, 'website'])
+  }
 }
--- src/app/components/website/website-list/website-list.component.ts
+++ src/app/components/website/website-list/website-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsiteService } from 'src/app/services/website.service.client';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-website-list',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-list.component.css']
 })
 export class WebsiteListComponent implements OnInit {
+  websites : any[]; // array that stores all of our websites
+  uid : string;
 
-  constructor() { }
 
+  // constructor will run BEFORE the page is loaded
+  constructor(private websiteService : WebsiteService, private activatedRoute : ActivatedRoute) {}
+  // Router will route user from one page to the next
+  // ActivatedRoute will let you access data in the URL
+
+  // ngOnInit function will run after the page has finished loading
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params) => {
+        this.uid = params['uid'];
+        this.websites = this.websiteService.findWebsitesByUser(this.uid);
+      }
+    )
   }
 
 }
