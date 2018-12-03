81405880b6a479febdf59376511184b5c49b4bd7
[Andisa Jones] <hmtran.ae@gmail.com>
2018-12-03 11:59:53 -0500

    connect to mongodb

--- server/model/models.server.js
+++ server/model/models.server.js
@@ -1,15 +1,14 @@
 
-var connectionString = 'mongodb://127.0.0.1:27017/web-dev'; // for local
+var connectionString = 'mongodb://127.0.0.1:27017/web-maker'; // for local
 
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds0000.mlab.com:29004/dasdasds'; // use yours
+  connectionString += '@ds251022.mlab.com:51022/heroku_rd2z6q6h'; // use yours
+  // Your connection string is found on Heroku's add-on mLab
 }
 
-var mongoose = require("mongoose");
-
-var db = mongoose.connect(connectionString);
-
-module.exports = db;
+var mongoose = require("mongoose"); // Import Mongoose library
+var db = mongoose.connect(connectionString, { useNewUrlParser: true }); // Connect server with db
+module.exports = db; // Export this file
