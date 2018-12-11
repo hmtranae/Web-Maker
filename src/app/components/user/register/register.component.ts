0c353d39718096ede6ef7266d8f13c944706488a
TSN Student <student@ulem.org>
2018-12-11 11:14:34 -0500

    register and admin authentication

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service.client';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model.client';
+import { SharedService } from 'src/app/services/shared.service.client';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +17,7 @@ export class RegisterComponent implements OnInit {
   passwordError : boolean;
   userError : boolean;
 
-  constructor(private userService : UserService, private router : Router) { }
+  constructor(private userService : UserService, private router : Router, private sharedService : SharedService) { }
 
   ngOnInit() {}
 
@@ -37,15 +38,19 @@ export class RegisterComponent implements OnInit {
             lastName : '',
             email : ''
           };
-          this.userService.createUser(newUser).subscribe(
-            (user : User) => {
-              this.router.navigate(['user', user._id]);
-            })
+          this.userService.register(newUser).subscribe(
+            (data : User) => {
+              this.sharedService.user = data;
+              this.router.navigate(['profile']);
+            },
+            (error : any) => {
+              this.userError = true;
+            }
+          );
           } else {
             this.userError = true;
           }
-        }
-      )
+        });
     }
   }
 }
